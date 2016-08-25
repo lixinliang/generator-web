2c73fb4593adb76669b80e7e79ad9dd896f961be
lixinliang <lixinliang@yy.com>
2016-08-25 14:11:21 +0800

    update

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,14 +1,62 @@
 'use strict';
+var fs = require('fs');
+var del = require('del');
+var path = require('path');
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+var log = console.log;
 
 module.exports = yeoman.generators.Base.extend({
-  writing: function () {
-    console.log('writing');
-  },
-  install: function () {
-    // this.installDependencies();
-    console.log('install');
-  }
+    constructor: function(){
+        yeoman.Base.apply(this, arguments);
+        this.conflicter.force = true;
+        if (fs.existsSync(path.join(process.cwd(), '/src'))) {
+            log('> 资源已初始化，退出...');
+            process.exit(1);
+        }
+    },
+    prompting: function(){
+        let done      = this.async();
+        let timestamp = +new Date();
+        let questions = [
+            {
+                name   : 'name',
+                message: '项目名称',
+                default: 'Project',
+            },
+            {
+                name   : 'version',
+                message: '项目版本号',
+                default: '0.0.1',
+            },
+            {
+                name   : 'description',
+                message: '项目描述',
+                default: '',
+            }
+        ];
+        this.prompt(questions, function (answers) {
+            for (let item in answers) {
+                answers.hasOwnProperty(item) && (this[item] = answers[item]);
+            }
+            done();
+        }.bind(this));
+    },
+    writing: function () {
+        this.directory('src', 'src');
+        this.directory('webpack', 'webpack');
+        this.copy('package.json', 'package.json');
+    },
+    install: function () {
+        del(['src/**/.gitignore','src/**/.npmignore']);
+        // this.installDependencies({
+        //     bower      : false,
+        //     // npm        : true,
+        //     npm        : false,
+        //     skipInstall: false,
+        //     callback   : () => {
+        //         log('> 初始化已完成');
+        //         process.exit(1);
+        //     }
+        // });
+    }
 });
