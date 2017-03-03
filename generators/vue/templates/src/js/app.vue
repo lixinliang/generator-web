187c8149e6d70d7b8fbd1d51457942ae68a71e6f
李昕亮 <lixinliang@yy.com>
2017-03-03 15:21:17 +0800

    update
    update

---
+++ generators/vue/templates/src/js/app.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    created () {
+        console.log('created');
+    },
+};
+</script>
+
+<template lang="html">
+    <div>test</div>
+</template>
+
+<style lang="scss" scoped>
+    div {
+        color: red;
+    }
+</style>
